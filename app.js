0a200e203ef8a0b7656f19744b112cf918037ec6
unknown <kparker4089@myhta.org>
2019-04-24 15:44:44 -1000

    done

---
+++ app.js
@@ -0,0 +1,27 @@
+var grateful = ["food", "water", "phone"];
+grateful.pop();
+console.log(grateful);
+
+grateful.push("family", "sports");
+console.log(grateful);
+
+grateful.shift();
+console.log(grateful);
+
+grateful.unshift("music");
+console.log(grateful);
+
+
+
+var proud = ["soccer", "perserverence"];
+proud.pop();
+console.log(proud);
+
+proud.push("sports", "hobbies");
+console.log(proud);
+
+proud.shift();
+console.log(proud);
+
+proud.unshift("proud.");
+console.log(proud);
